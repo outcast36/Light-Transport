5d8e60c098e452c9fc2bb21b7a19ca487e6d3280
outcast36 <wyattayers43@gmail.com>
2024-08-19 16:03:05 -0700

    Signature revisions

--- src/main/FileHandle.cpp
+++ src/main/FileHandle.cpp
@@ -1,28 +1,27 @@
-#include <stdexcept>
-#include <cstdio>
 #include "FileHandle.h"
 
 FileHandle::FileHandle(const char * file_name, const char * mode){
     f = fopen(file_name, mode);
-    if (!f) throw std::runtime_error("Failed to open file");
+    if (!f) std::perror("Failed to open file");
 }
 
 FileHandle::~FileHandle(){
     fclose(f);
 }
 
-size_t FileHandle::readFile(char * buffer, size_t size)
-{
-    size_t read_count = fread(buffer, 1, size, f);
+size_t FileHandle::readFile(char* buffer, size_t size, size_t count) {
+    size_t read_count = fread(buffer, size, count, f); // read given size parameter bytes
     // ferror returns a nonzero value if the given file stream has any errors
-    if ((read_count != size) && ferror(f)) throw std::runtime_error("Failed to read from file");
+    // fread returns count of objects read successfully
+    if ((read_count != count) && ferror(f)) std::perror("Failed to read from file");
+    else if (read_count != count && this->is_eof()) std::perror("Read to EOF successfully");
     return read_count;
 }
 
-void FileHandle::writeFile(const char * buffer, size_t size)
+void FileHandle::writeFile(const char* buffer, size_t size, size_t count)
 {
-    size_t write_count = fwrite(buffer, 1, size, f);
-    if (write_count != size) throw std::runtime_error("Failed to write to file");
+    size_t write_count = fwrite(buffer, size, count, f);
+    if (write_count != count && ferror(f)) std::perror("Failed to write to file");
 }
 
 bool FileHandle::is_eof(){
