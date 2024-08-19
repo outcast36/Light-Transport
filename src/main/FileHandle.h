4f3a65f12e4c413fdceb6091ae8f68c743c10345
outcast36 <wyattayers43@gmail.com>
2024-08-19 15:58:34 -0700

    Changed read/write signatures to have both size and count params

--- src/main/FileHandle.h
+++ src/main/FileHandle.h
@@ -12,10 +12,10 @@ class FileHandle {
         // Delete these two so destructor doesn't try to release the same resource twice
         // https://bajamircea.github.io/coding/cpp/2015/03/17/classic-raii.html
         FileHandle(const FileHandle &) = delete; //copy constructor
-        FileHandle & operator=(const FileHandle &) = delete; //assignment operator
+        FileHandle& operator=(const FileHandle &) = delete; //assignment operator
         
-        size_t readFile(char * buffer, size_t size);
-        void writeFile(const char * buffer, size_t size);
+        size_t readFile(char* buffer, size_t size, size_t count);
+        void writeFile(const char* buffer, size_t size, size_t count);
         bool is_eof();
     private: 
         FILE *f;
