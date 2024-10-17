e3e46be3e612869771dc4d79a053cd1dba5af8f1
outcast36 <wyattayers43@gmail.com>
2024-10-16 21:54:58 -0700

    Added deodorant

--- src/main/FileHandle.cpp
+++ src/main/FileHandle.cpp
@@ -2,43 +2,49 @@
 #include "FileHandle.h"
 
 // Acquire file on construction
-FileHandle::FileHandle(const char * file_name, const char * mode){
+FileHandle::FileHandle(const char * file_name, const char * mode) {
     f = fopen(file_name, mode);
     if (!f) throw new std::runtime_error("Failed to open file");
 }
 
 // Release file on destruction
-FileHandle::~FileHandle(){
+FileHandle::~FileHandle() {
     fclose(f);
 }
 
-// Read an arbitrary number of objects which contain san arbitrary amount of bytes
+// Read an arbitrary number of objects which contain can arbitrary amount of bytes
 size_t FileHandle::readFile(char* buffer, size_t size, size_t count) {
     size_t read_count = fread(buffer, size, count, f); // read given size parameter bytes
+
     // ferror returns a nonzero value if the given file stream has any errors
     // fread returns count of objects read successfully
-    if ((read_count != count) && ferror(f)) throw new std::runtime_error("Failed to read from file.");
+    if ((read_count != count) && ferror(f)) {
+        throw new std::runtime_error("Failed to read from file.");
+    } 
 
     // In the EOF case, return number of bytes to callervoid
     return read_count;
 }
 
 // Read a line delimited by newline from the file
-size_t FileHandle::readLine(char* buffer){
-    size_t line_size=128;
-    if (getline(&buffer, &line_size, f)<0) std::perror("Failed to get line");
+size_t FileHandle::readLine(char* buffer) {
+    size_t buffer_size=4096, line_size;
+    if (line_size=getline(&buffer, &buffer_size, f)<0) {
+        std::perror("Failed to get line");
+    }
     return line_size;
 }
 
 // Write arbitrary amount of objects which contain an arbitrary amount of bytes
-size_t FileHandle::writeFile(const char* buffer, size_t size, size_t count)
-{
+size_t FileHandle::writeFile(const char* buffer, size_t size, size_t count) {
     size_t write_count = fwrite(buffer, size, count, f);
-    if (write_count != count && ferror(f)) throw new std::runtime_error("Failed to write to file");
+    if (write_count != count && ferror(f)) {
+        throw new std::runtime_error("Failed to write to file");
+    } 
     return write_count;
 }
 
 // Check file EOF status
-bool FileHandle::is_eof(){
+bool FileHandle::is_eof() {
     return feof(f);
 }
