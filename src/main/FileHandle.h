e4aab4580a11b2c5f6b68ec7e87c6d667bf31336
outcast36 <wyattayers43@gmail.com>
2024-12-12 21:39:51 -0800

    Change return types to handle exception removal

--- src/main/FileHandle.h
+++ src/main/FileHandle.h
@@ -14,9 +14,9 @@ class FileHandle {
         FileHandle(const FileHandle &) = delete; //copy constructor
         FileHandle& operator=(const FileHandle &) = delete; //assignment operator
         
-        size_t readFile(void* buffer, size_t size, size_t count);
-        size_t readLine(char* buffer);
-        size_t writeFile(const void* buffer, size_t size, size_t count);
+        int32_t readFile(void* buffer, size_t size, size_t count);
+        int32_t readLine(char* buffer);
+        int32_t writeFile(const void* buffer, size_t size, size_t count);
         bool is_eof();
     private: 
         FILE *f;
