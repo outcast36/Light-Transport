9ac0c18a0481f90030eb753b3e98d317688e9fd9
outcast36 <wyattayers43@gmail.com>
2024-12-12 19:59:38 -0800

    Remove exceptions except from constructor

--- src/main/FileHandle.cpp
+++ src/main/FileHandle.cpp
@@ -18,9 +18,7 @@ size_t FileHandle::readFile(void* buffer, size_t size, size_t count) {
 
     // ferror returns a nonzero value if the given file stream has any errors
     // fread returns count of objects read successfully
-    if ((read_count != count) && ferror(f)) {
-        throw new std::runtime_error("Failed to read from file.");
-    } 
+    if ((read_count != count) && ferror(f)) return -1;
 
     // In the EOF case, return number of bytes to caller
     return read_count;
@@ -28,9 +26,10 @@ size_t FileHandle::readFile(void* buffer, size_t size, size_t count) {
 
 // Read a line delimited by newline from the file
 size_t FileHandle::readLine(char* buffer) {
-    size_t buffer_size=4096, line_size;
-    if (line_size=getline(&buffer, &buffer_size, f)<0) {
-        std::perror("Failed to get line");
+    size_t buffer_size=129, line_size;
+    if (fgets(buffer, buffer_size, f)==NULL) {
+        std::perror("Failed to read line from file");
+        return -1;
     }
     return line_size;
 }
@@ -39,7 +38,8 @@ size_t FileHandle::readLine(char* buffer) {
 size_t FileHandle::writeFile(const void* buffer, size_t size, size_t count) {
     size_t write_count = fwrite(buffer, size, count, f);
     if (write_count != count && ferror(f)) {
-        throw new std::runtime_error("Failed to write to file");
+        std::perror("Failed to write to file");
+        return -1;
     } 
     return write_count;
 }
