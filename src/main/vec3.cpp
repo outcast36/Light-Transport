15f8069665fe72f113550b63074374783faf5c6b
outcast36 <wyattayers43@gmail.com>
2024-12-12 21:08:46 -0800

    Template vec3 implementation for OpenEXR

--- src/main/vec3.cpp
+++ src/main/vec3.cpp
@@ -1,94 +1,94 @@
 #include <cmath>
 #include "vec3.h"
 
-vec3::vec3() : v{0,0,0} {};
-vec3::vec3(double x, double y, double z) : v{x,y,z} {};
+template <typename T> vec3<T>::vec3() : v{0,0,0} {};
+template <typename T> vec3<T>::vec3(T x, T y, T z) : v{x,y,z} {};
 
-double vec3::x() const {
+template <typename T> T vec3<T>::x() const {
     return v[0];
 }
 
-double vec3::y() const {
+template <typename T> T vec3<T>::y() const {
     return v[1];
 }
 
-double vec3::z() const {
+template <typename T> T vec3<T>::z() const {
     return v[2];
 }
 
-vec3 vec3::operator-() const {
+template <typename T> vec3<T> vec3<T>::operator-() const {
     return {-v[0], -v[1], -v[2]};
 }
 
-vec3& vec3::operator+=(const vec3& w) {
+template <typename T> vec3<T>& vec3<T>::operator+=(const vec3<T>& w) {
     v[0] += w.v[0];
     v[1] += w.v[1];
     v[2] += w.v[2];
     return *this;
 }
 
-vec3& vec3::operator*=(double t) {
+template <typename T> vec3<T>& vec3<T>::operator*=(T t) {
     v[0] *= t;
     v[1] *= t;
     v[2] *= t;
     return *this;
 }
 
-vec3& vec3::operator/=(double t) {
+template <typename T> vec3<T>& vec3<T>::operator/=(T t) {
     return *this *= (1/t);
 }
 
-double vec3::operator[](int i) const {
+template <typename T> T vec3<T>::operator[](int i) const {
     return v[i];
 }
 
-double& vec3::operator[](int i) {
+template <typename T> T& vec3<T>::operator[](int i) {
     return v[i];
 }
 
-double vec3::length() const {
+template <typename T> T vec3<T>::length() const {
     return sqrt(length_squared());
 }
 
-double vec3::length_squared() const {
+template <typename T> T vec3<T>::length_squared() const {
     return v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
 }
 
 
-vec3 operator*(double t, const vec3& v) {
-    return vec3(t*v.x(), t*v.y(), t*v.z());
+template <typename T> vec3<T> operator*(T t, const vec3<T>& v) {
+    return vec3<T>(t*v.x(), t*v.y(), t*v.z());
 }
 
-vec3 operator*(const vec3& v, double t) {
+template <typename T> vec3<T> operator*(const vec3<T>& v, T t) {
     return t * v;
 }
 
-vec3 operator*(const vec3& v, const vec3& w) {
-    return vec3(v.x()*w.x(), v.y()*w.y(), v.z()*w.z());
+template <typename T> vec3<T> operator*(const vec3<T>& v, const vec3<T>& w) {
+    return vec3<T>(v.x()*w.x(), v.y()*w.y(), v.z()*w.z());
 }
 
-vec3 operator+(const vec3& v, const vec3& w) {
-    return vec3(v.x()+w.x(), v.y()+w.y(), v.z()+w.z());
+template <typename T> vec3<T> operator+(const vec3<T>& v, const vec3<T>& w) {
+    return vec3<T>(v.x()+w.x(), v.y()+w.y(), v.z()+w.z());
 }
 
-vec3 operator-(const vec3& v, const vec3& w) {
-    return vec3(v.x()-w.x(), v.y()-w.y(), v.z()-w.z());
+template <typename T> vec3<T> operator-(const vec3<T>& v, const vec3<T>& w) {
+    return vec3<T>(v.x()-w.x(), v.y()-w.y(), v.z()-w.z());
 }
 
-vec3 operator/(const vec3& v, double t) {
+template <typename T> vec3<T> operator/(const vec3<T>& v, T t) {
     return (1/t) * v;
 }
 
 // Algebraic definition of cross product: v X w = (v2w3 + v3w2)i + (v3w1 - v1w3)j + (v1w2 - v2w1)k
-vec3 cross(const vec3& v, const vec3& w) {
-    return vec3(v.y()*w.z()+v.z()*w.y(), v.z()*w.x()-v.x()*w.z(), v.x()*w.y()-v.y()*w.x());
+template <typename T> vec3<T> cross(const vec3<T>& v, const vec3<T>& w) {
+    return vec3<T>(v.y()*w.z()+v.z()*w.y(), v.z()*w.x()-v.x()*w.z(), v.x()*w.y()-v.y()*w.x());
 }
 
-vec3 unit_vector(const vec3& v) {
+template <typename T> vec3<T> unit_vector(const vec3<T>& v) {
     return v / v.length();
 }
 
-double dot(const vec3& v, const vec3& w) {
+template <typename T> T dot(const vec3<T>& v, const vec3<T>& w) {
     return v.x()*w.x() + v.y()*w.y() + v.z()*w.z();
 }
 
