a3bdf49c02b2c2dc8b3d5bbc412cc422ae2cdf46
outcast36 <wyattayers43@gmail.com>
2024-12-24 12:58:21 -0800

    Empty constructor and read/write functions changed

--- src/main/PFMImageIO.h
+++ src/main/PFMImageIO.h
@@ -19,9 +19,10 @@ class PFMImage {
         int32_t readPFMHeader(FileHandle& fh);
         int32_t writePFMHeader(FileHandle& fh);
     public:
-        PFMImage(uint32_t img_width, uint32_t img_height, float pixel_max, bool RGB);
-        int32_t readPFM(PFMImage* img,  std::string infile);    
-        int32_t writePFM(PFMImage* img,  std::string outfile);
+        PFMImage(); // empty constructor for testing read/write PFM
+        PFMImage(uint32_t img_width, uint32_t img_height, float pixel_max, bool RGB); // when rendering a PFM image
+        int32_t readPFM(std::string infile);    
+        int32_t writePFM(std::string outfile);
         void writePixel(uint32_t i, uint32_t j);
 };
 
