095c8fcda03cb5c021b5932abeb88ff500d70baf
outcast36 <wyattayers43@gmail.com>
2024-12-24 12:58:40 -0800

    Almost done with PFM class convert

--- src/main/PFMImageIO.cpp
+++ src/main/PFMImageIO.cpp
@@ -70,23 +70,23 @@ int32_t PFMImage::readPFMHeader(FileHandle& fh) {
     n = fh.readLine(&buf[0]);
     if (n<0) return -1;
     pixel_max = std::stof(buf);
-    img->img_width = width;
-    img->img_height = height;
-    img->pixel_max = std::abs(pixel_max);
-    img->RGB = color;
+    this->img_width = width;
+    this->img_height = height;
+    this->pixel_max = std::abs(pixel_max);
+    this->RGB = color;
     return 0;
 }
 
 // Read a PFM image in its entirety from the given path: infile.pfm
 // Data is output into the pointer img
-int32_t PFMImage::readPFM(PFMImage* img,  std::string infile) {
+int32_t PFMImage::readPFM(std::string infile) {
     uint32_t width, height;
     int32_t n;
     float pixel[3]; // RGB channel value buffer
     FileHandle fh = FileHandle(infile.c_str(), "r");
-    n=readPFMHeader(fh,img);
+    n=readPFMHeader(fh);
     if(n<0) return -1;
-    width = img->img_width; height = img->img_height;
+    width = this->img_width; height = this->img_height;
     pixelMap pixels(height, std::vector<rgb32>(width));
     for (uint32_t i=0;i<height;i++) { // numbered 0 to height from bottom to top
         for (uint32_t j=0;j<width;j++) { // numbered 0 to width from left to right
@@ -95,7 +95,7 @@ int32_t PFMImage::readPFM(PFMImage* img,  std::string infile) {
             pixels[i][j]=vec3<float>(pixel[0], pixel[1], pixel[2]);
         }
     }
-    img->img_array = std::make_unique<pixelMap>(pixels);
+    this->img_array = std::make_unique<pixelMap>(pixels);
     return 0; // destructor of FileHandle instance invoked on return 
 }
 
