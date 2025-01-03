2fd2fedd526ce2934e8343e7c000dbf9c598dc20
outcast36 <wyattayers43@gmail.com>
2025-01-02 19:42:01 -0800

    Header file with some color RGB color definitions

---
+++ src/main/colors.h
@@ -0,0 +1,12 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#include "vec3.h"
+
+vec3<float> lime(0.33, 0.97, 0.26);
+vec3<float> white(1,1,1);
+vec3<float> black(0,0,0);
+vec3<float> red(1,0,0);
+vec3<float> blue(0,0,1);
+
+#endif /* COLORS_H */
