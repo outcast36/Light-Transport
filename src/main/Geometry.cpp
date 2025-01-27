d73d2c237ef939fbc092824e4d276f1baf7da190
outcast36 <wyattayers43@gmail.com>
2025-01-27 14:08:17 -0800

    Cylinder intersection using point-line distance

--- src/main/Geometry.cpp
+++ src/main/Geometry.cpp
@@ -38,3 +38,31 @@ int32_t Plane::rayIntersect(Collision* hit, Ray ray) {
     hit->surface_normal = this->normal;
     return 0;
 }
+
+int32_t Cylinder::rayIntersect(Collision* hit, Ray ray) {
+    vec3<double> axis_to_origin = ray.origin - this->point_in_center;
+    vec3<double> scaled_rotation_axis = dot(ray.direction, axis_of_rotation) * this->axis_of_rotation;
+    double radius_squared = this->radius * this->radius;
+    double inverse_radius = 1/this->radius;
+    double regularization = dot(axis_of_rotation, axis_to_origin) * dot(axis_of_rotation, axis_to_origin);
+    double a = dot(ray.direction, ray.direction - this->axis_of_rotation);
+    double inverse_a = 1/a;
+    double half_b = dot(axis_to_origin, ray.direction - scaled_rotation_axis);
+    double c = dot(axis_to_origin, axis_to_origin) - regularization - radius_squared;
+    double discriminant = (half_b * half_b) - (a * c); 
+
+    if (discriminant < 0) return -1; // no intersection
+    // the closest interesection is with the smallest positive solution for t
+    double t = ((-half_b) - sqrt(discriminant)) * inverse_a;
+    // only compute t1 (the + solution to quadratic) if the other solution is negative
+    if (t < 0) t = ((-half_b) + sqrt(discriminant)) * inverse_a; 
+    vec3<double> intersection;
+    intersection = ray.origin + (t * ray.direction);
+    vec3<double> intersection_to_axis = intersection - point_in_center;
+    double projection_length = dot(intersection_to_axis, this->axis_of_rotation);
+    vec3<double> orthogonal_component = intersection_to_axis - (projection_length * this->axis_of_rotation);
+    hit->intersection = intersection;
+    hit->surface_normal = orthogonal_component * inverse_radius;
+    hit->t = t;
+    return 0;
+}
