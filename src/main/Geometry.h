b44b87db5a0cf22633c2065b37a6f242a9cad93e
outcast36 <wyattayers43@gmail.com>
2025-02-04 19:01:01 -0800

    Add ray intersect interval to all shapes

--- src/main/Geometry.h
+++ src/main/Geometry.h
@@ -10,7 +10,7 @@
 // Abstract shape class, shapes and surfaces must be able to be intersected by a ray
 class Geometry {
     public:
-        virtual int32_t rayIntersect(Collision* hit, Ray ray)=0; // pure virtual function
+        virtual int32_t rayIntersect(Collision* hit, Ray ray, float tmin, float tmax)=0; // pure virtual function
 };
 
 // Class for list of geometric objects, wrap the closest intersection method as a 
@@ -18,31 +18,30 @@ class Geometry {
 class Scene : public Geometry {
     public:
         Scene();
-        int32_t rayIntersect(Collision* hit, Ray ray);
-        void clearScene();
+        int32_t rayIntersect(Collision* hit, Ray ray, float tmin, float tmax);
         void add(std::shared_ptr<Geometry>); // push_back wrapper
+        void clearScene();
         std::vector<std::shared_ptr<Geometry>> geometry_list; // list of objects in the scene
-
 };
 
 class Sphere : public Geometry {
     public:
         Sphere(vec3<double>& center, double radius);
-        int32_t rayIntersect(Collision* hit, Ray ray);
+        int32_t rayIntersect(Collision* hit, Ray ray, float tmin, float tmax);
         vec3<double> center;
         double radius;
 };
 
 class Plane : public Geometry {
     public:
-        int32_t rayIntersect(Collision* hit, Ray ray);
+        int32_t rayIntersect(Collision* hit, Ray ray, float tmin, float tmax);
         vec3<double> point;
         vec3<double> normal;
 };
 
 class Cylinder : public Geometry {
     public: 
-        int32_t rayIntersect(Collision* hit, Ray ray);
+        int32_t rayIntersect(Collision* hit, Ray ray, float tmin, float tmax);
         vec3<double> axis_of_rotation; // normal vector for some cross sectional plane of the cylinder
         vec3<double> point_in_center; // point on line forming axis of rotation
         double height;
