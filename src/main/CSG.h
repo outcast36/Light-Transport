60e8c8edb25768078a22a6838546415ac8c9b049
outcast36 <wyattayers43@gmail.com>
2025-03-11 15:42:07 -0700

    Ray intersect uses interval, remove interval field

--- src/main/CSG.h
+++ src/main/CSG.h
@@ -8,16 +8,21 @@
 #include "vec3.h"
 #include "Interval.h"
 
+enum SetOperation{unions, intersect, difference};
+
 // Store a single node of the CSG tree with two objects to combine
 class CSG : public Geometry {
     public:
         CSG();
-        int32_t rayIntersect(Collision* hit, Ray ray, float tmin, float tmax);
+        int32_t rayIntersect(Collision* hit, Ray ray, Interval& range);
     private:
         // Consider making the left and right operands Geometry& ?? 
         std::shared_ptr<Geometry> left;
         std::shared_ptr<Geometry> right; 
-        Interval range; // empty if both left and right are geometric primitives
+        SetOperation op;
 };
 
 #endif /* CSG_H */
+
+//Structure of arbitrary csg object
+//CSG( CSG(sphere, cube, intersect), CSG( CSG( cylinder, cylinder, unions), cylinder, unions), difference)
