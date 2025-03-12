4a96cca886f507d27ef0c26aa8adac95935d3449
outcast36 <wyattayers43@gmail.com>
2025-03-11 22:32:38 -0700

    Test scenes for union and difference operations

--- src/main/TestScenes.h
+++ src/main/TestScenes.h
@@ -1,16 +1,17 @@
 #ifndef TESTSCENES_H
 #define TESTSCENES_H
 
-#include <cstdint>
 #include <cmath>
 #include <memory>
-#include <vector>
 #include "Geometry.h"
 #include "vec3.h"
 #include "Interval.h"
+#include "CSG.h"
 
 Scene scene1();
 Scene scene2();
 Scene scene3();
+Scene scene4();
+Scene scene5();
 
 #endif /* TESTSCENES_H */
