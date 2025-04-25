f89ecce9eb97334a821e72d885a0c31f6c3384a2
outcast36 <wyattayers43@gmail.com>
2025-04-24 18:32:02 -0700

    Remove span struct and interval include

--- src/object/GFXBase.h
+++ src/object/GFXBase.h
@@ -2,20 +2,12 @@
 #define GFXBASE_H
 
 #include "math/vec3.h"
-#include "math/Interval.h"
 
 // Data structure for a single point of intersection with a surface
 struct Collision {
     vec3<double> intersection;
     vec3<double> surface_normal;
     double t;
-};
-
-// Structure for a single intersection interval with an object
-struct Span {
-    Collision entry;
-    Collision exit;
-    Span* next; // linked list chaining multiple intersection intervals together
 };
 
 struct Ray {
