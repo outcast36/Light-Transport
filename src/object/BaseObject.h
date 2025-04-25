ed0e9b0769466604d1b5960ba4bfc92bcd8470a9
outcast36 <wyattayers43@gmail.com>
2025-04-24 18:30:11 -0700

    Add virtual destructor to abstract base object class

--- src/object/BaseObject.h
+++ src/object/BaseObject.h
@@ -2,12 +2,14 @@
 #define BASEOBJECT_H
 
 #include <cstdint>
-#include "GFXBase.h"
+#include "object/GFXBase.h"
+#include "object/Span.h"
 
-// Abstract shape class, shapes and surfaces must be able to be intersected by a ray
+// Abstract class for primitives and CSG objects which require full intersection data
 class BaseObject {
     public:
         virtual int32_t rayIntersect(Span* hit, Ray ray, Interval range)=0; // pure virtual
+        virtual ~BaseObject() = default;
 };
 
 #endif /* BASEOBJECT_H */
