4ee4950b322cf6f52f9f08dbfc3fd20ed176b90f
outcast36 <wyattayers43@gmail.com>
2025-04-24 18:31:26 -0700

    Add interface for intersection queries which only care about a single intersection

---
+++ src/scene/RaycastTarget.h
@@ -0,0 +1,15 @@
+#ifndef RAYCASTTARGET_H
+#define RAYCASTTARGET_H
+
+#include <cstdint>
+#include "object/GFXBase.h"
+#include "math/Interval.h"
+
+// Abstract class for primitives and CSG objects which require full intersection data
+class RaycastTarget {
+    public:
+        virtual int32_t rayIntersect(Collision* hit, Ray ray, Interval range)=0; // pure virtual
+        virtual ~RaycastTarget() = default;
+};
+
+#endif /* RAYCASTTARGET_H */
