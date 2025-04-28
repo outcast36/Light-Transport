e2c8b3bbe3aeb02bc60fecfdbf8abd4608c30f3e
outcast36 <wyattayers43@gmail.com>
2025-04-27 18:29:28 -0700

    Adjust style to follow Google guidelines

--- src/image/FileHandle.h
+++ src/image/FileHandle.h
@@ -5,21 +5,20 @@
 #include <cstdio>
 
 class FileHandle {
-    public:
-        FileHandle(const char * file_name, const char * mode);
-        ~FileHandle();
+ public:
+  FileHandle(const char * file_name, const char * mode);
+  ~FileHandle();
+  // Delete these two so destructor doesn't try to release the same resource twice
+  // https://bajamircea.github.io/coding/cpp/2015/03/17/classic-raii.html
+  FileHandle(const FileHandle &) = delete; //copy constructor
+  FileHandle& operator=(const FileHandle &) = delete; //assignment operator   
+  int32_t readFile(void* buffer, size_t size, size_t count);
+  int32_t readLine(char* buffer);
+  int32_t writeFile(const void* buffer, size_t size, size_t count);
+  bool is_eof();
 
-        // Delete these two so destructor doesn't try to release the same resource twice
-        // https://bajamircea.github.io/coding/cpp/2015/03/17/classic-raii.html
-        FileHandle(const FileHandle &) = delete; //copy constructor
-        FileHandle& operator=(const FileHandle &) = delete; //assignment operator
-        
-        int32_t readFile(void* buffer, size_t size, size_t count);
-        int32_t readLine(char* buffer);
-        int32_t writeFile(const void* buffer, size_t size, size_t count);
-        bool is_eof();
-    private: 
-        FILE *f;
+ private: 
+  FILE *f;
 };
 
 #endif /* FILEHANDLE_H */
--- src/image/PFMImageIO.h
+++ src/image/PFMImageIO.h
@@ -10,19 +10,20 @@ using rgb32 = vec3<float>;
 using pixelMap = std::vector<std::vector<rgb32>>; // switch to std::array
 
 class PFMImage {
-    private:
-        uint32_t img_width;
-        uint32_t img_height;
-        float pixel_max;
-        bool RGB; // true if image is tri-channel
-        int32_t readPFMHeader(FileHandle& fh);
-        int32_t writePFMHeader(FileHandle& fh);
-    public:
-        std::unique_ptr<pixelMap> img_array; // <- woag free memory management
-        PFMImage(); // empty constructor for testing read/write PFM
-        PFMImage(uint32_t img_width, uint32_t img_height, float pixel_max, bool RGB); // when rendering a PFM image
-        int32_t readPFM(std::string infile);    
-        int32_t writePFM(std::string outfile);
+ public:
+  std::unique_ptr<pixelMap> img_array; // <- woag free memory management
+  PFMImage(); // empty constructor for testing read/write PFM
+  PFMImage(uint32_t img_width, uint32_t img_height, float pixel_max, bool RGB); // when rendering a PFM image
+  int32_t readPFM(std::string infile);    
+  int32_t writePFM(std::string outfile);
+
+ private:
+  uint32_t img_width;
+  uint32_t img_height;
+  float pixel_max;
+  bool RGB; // true if image is tri-channel
+  int32_t readPFMHeader(FileHandle& fh);
+  int32_t writePFMHeader(FileHandle& fh);
 };
 
 #endif /* PFMIMAGEIO_H */
