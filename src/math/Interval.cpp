307459360a7b295b4fd308b2421d83924044034e
outcast36 <wyattayers43@gmail.com>
2025-05-01 17:01:11 -0700

    Remove binary interval set operations for CSG redesign

--- src/math/Interval.cpp
+++ src/math/Interval.cpp
@@ -1,6 +1,5 @@
 #include <limits>
 #include <algorithm>
-#include <iostream>
 #include "Interval.h"
 
 // Empty constructor creates empty interval
@@ -22,39 +21,4 @@ bool Interval::contains(double t) const {
 // Check if t in (min, max)
 bool Interval::exclusiveContains(double t) const {
     return this->min < t && t < this->max;
-}; 
-
-Interval unionInterval(Interval& a, Interval& b) {
-    if (a.empty()) return b;
-    else if (b.empty()) return a;
-    else return Interval(std::min(a.min, b.min), std::max(a.max, b.max));
-}
-
-Interval intersectInterval(Interval& a, Interval& b) {
-    double start = std::max(a.min, b.min);
-    double end = std::min(a.max, b.max);
-    return Interval(start, end);
-}
-
-// A - B
-Interval differenceInterval(Interval& a, Interval& b) {
-    if (a.empty() || b.empty() || intersectInterval(a, b).empty()) return a; 
-    // Return empty interval when A interval is equal to or inside of B interval
-    if (a.min >= b.min && a.max <= b.max) return Interval();
-    // Difference interval A - B is completely to the right of B interval
-    else if (a.min >= b.min && a.max > b.max) return Interval(b.max, a.max);
-    // Difference interval A - B is completely to the left of B interval
-    else if (a.min < b.min && a.max <= b.max) return Interval(a.min, b.min);
-
-
-
-    // Difference interval A - B splits the A interval into two sections
-    // Return closer of the two difference intervals with a min value > 0
-    else {
-        if (a.min > 0) return Interval(a.min, b.min);
-        else return Interval(a.max, b.max);
-    }
-}
-
-// Merge intervals together and remove any surfaces inside the boundary of a union operation
-// Interval mergeInterval(Interval& a, Interval& b) 
+};
