341b598b41573e6f8f67c30c46439e86af996abb
outcast36 <wyattayers43@gmail.com>
2025-05-01 17:00:47 -0700

    Style cleanup and better design

--- src/object/Span.h
+++ src/object/Span.h
@@ -1,25 +1,21 @@
 #ifndef SPAN_H
 #define SPAN_H
 
-#include <memory>
-#include <vector>
-#include <array>
-#include "math/Interval.h"
 #include "object/GFXBase.h"
 
-// Span -- Structure for intersection intervals for a single ray intersection query 
-// Invariant: For any intersection query that requires storage of three of more intersection intervals
-// will create and use the vector disjointIntervals. The closest two intervals will always be stored in 
-// the array, and every thing else will be stored in the vector. Both storage containers will be in 
-// sorted order based on the lower bound values of their intervals.  
+// Data structure for storing surface data about a single intersection interval
+// Invariant: entry.t <= exit.t 
 class Span {
-    private: 
-        std::array<IntervalCollision, 2> inlineIntervals; // common case of one or two intervals
-        std::shared_ptr<std::vector<IntervalCollision>> extraIntervals; // shared pointer for lazy initialization
-        size_t intervalCount;
-        bool disjoint; // true if span is representing multiple disjoint intervals
-    public: 
-        void addInterval(Collision& a, Collision& b);
+ public:
+  Span(const Collision& entry, const Collision& exit) 
+      : entry(entry), exit(exit) {}
+  static Span invalid() {
+      Collision dummy{vec3<double>(), vec3<double>(), 0.0};
+      return Span(dummy, dummy);
+  }
+ private: 
+  Collision entry;
+  Collision exit;
 };
 
 #endif /* SPAN_H */
