d3a7b45ad815b0ad1864a4e8e75419b362f049a1
outcast36 <wyattayers43@gmail.com>
2025-05-01 17:05:06 -0700

    Style cleanup for sphere header

--- src/object/Sphere.h
+++ src/object/Sphere.h
@@ -5,11 +5,13 @@
 #include "BaseObject.h"
 
 class Sphere : public BaseObject {
-    public:
-        Sphere(vec3<double>& center, double radius);
-        int32_t rayIntersect(Span* hit, Ray ray, Interval range);
-        vec3<double> center;
-        double radius;
+ public:
+  Sphere(vec3<double>& center, double radius);
+  std::optional<IntervalSet> rayIntersect(Ray& ray, Interval range) override;
+ 
+ private: 
+  vec3<double> center;
+  double radius;
 };
 
 #endif /* SPHERE_H */
