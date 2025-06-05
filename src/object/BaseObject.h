70a5e672df82b46412450bec46d9fb806266320b
outcast36 <wyattayers43@gmail.com>
2025-06-04 22:08:35 -0700

    Reworking object API to defer (tmin,tmax) interval check

--- src/object/BaseObject.h
+++ src/object/BaseObject.h
@@ -3,14 +3,15 @@
 
 #include <cstdint>
 #include <optional>
+#include <vector>
 #include "GFXBase.h"
+#include "Span.h"
 #include "Interval.h"
-#include "IntervalSet.h"
 
 // Abstract class for primitives and CSG objects which require full intersection data
 class BaseObject {
  public:
-  virtual std::optional<IntervalSet> rayIntersect(Ray& ray, Interval range)=0; // pure virtual
+  virtual std::optional<std::vector<Span>> rayIntersect(Ray& ray)=0; // pure virtual
   virtual ~BaseObject() = default;
 };
 
