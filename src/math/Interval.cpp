bf2b593f764b97a7e1e678f884cbbcd0434b9416
outcast36 <wyattayers43@gmail.com>
2025-06-06 12:38:11 -0700

    Constructor style and fix variable names

--- src/math/Interval.cpp
+++ src/math/Interval.cpp
@@ -3,7 +3,10 @@
 #include "Interval.h"
 
 // Empty constructor creates empty interval
-Interval::Interval() : min(std::numeric_limits<double>::max()), max(-std::numeric_limits<double>::max()) {};
+Interval::Interval() 
+    : start(std::numeric_limits<double>::max()), 
+      end(-std::numeric_limits<double>::max()) {};
+      
 Interval::Interval(double start, double end) : start(start), end(end) {};
 double Interval::size() const {
     return this->end - this->start;
