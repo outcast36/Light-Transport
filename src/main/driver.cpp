33f0f26a52612837cfede366e2bcc2074b43fa99
outcast36 <wyattayers43@gmail.com>
2025-06-17 16:35:16 -0700

    Update driver for testing CSG union

--- src/main/driver.cpp
+++ src/main/driver.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
-#include <vector>
 #include <chrono>
-#include "image/FileHandle.h"
-#include "image/PFMImageIO.h"
-#include "math/vec3.h"
-#include "object/BaseObject.h"
-#include "object/GFXBase.h"
-#include "scene/Scene.h"
-#include "scene/Cameras.h"
-#include "scene/colors.h"
+#include "PFMImageIO.h"
+#include "BaseObject.h"
+#include "GFXBase.h"
+#include "Scene.h"
+#include "Cameras.h"
+#include "colors.h"
 #include "TestScenes.h"
 
 /*
 Main ray tracing render loop: determine color for each pixel
 */ 
 int main(int argc, char *argv[]){
-    //usage: <executable> rendering_alg=[whitted, distributed, monte carlo (bidirectional), mlt, photon mapping] width sample_count, scene
-    //./a.out whitted pfm 512 100 scene //(scene contains objects and camera?)
+    //usage: <executable> <rendering algorithm> width sample_count, scene
     if (argc < 2) {
         std::perror("Invalid Argument. Usage is: [executable_name] [img-out-path]");
         exit(EXIT_FAILURE);
     }    
     
     // Set up scene with camera object, light direction, and geometric objects
-    Scene scene_objects = scene1();
+    Scene scene_objects = scene2();
     PerspectiveCamera camera(480,480);
-    Span hit; // hit object to feed to Scene.rayIntersect to see if ray(i,j) intersects the scene at all
     vec3<double> light(0,-1,-1); light = unitVector(light);
-    vec3<float> color;
-    Interval hitRange(0.0, MAXFLOAT);
-    int32_t hit_status = -1;
-    float normal_factor = 0.5;
     
     // render_image is owner of unique_ptr<> which holds the array of 
     // floating point data representing the rgb pixel values
@@ -43,12 +34,15 @@ int main(int argc, char *argv[]){
     for (uint32_t i=0;i<480;i++) { // numbered 0 to height from bottom to top
         for (uint32_t j=0;j<480;j++) { // numbered 0 to width from left to right
             //std::cout << "Row: " << i << " Column: " << j << '\n';
-            color = black;
+            vec3<float> color = black;
             Ray ray = camera.pixelToRay(i,j);
-            //std::cout << "Ray direction: " << ray.direction.x() << " " << ray.direction.y() << " " << ray.direction.z() << std::endl;
-            hit_status = scene_objects.rayIntersect(&hit, ray, hitRange);
-            if (hit_status==0) {
-                // color pixel based on material properties of surface
+            auto hit_status = scene_objects.rayIntersect(ray, Interval(0.0, MAXFLOAT));
+            if (hit_status) {
+                Collision hit = hit_status.value();
+                //std::cout << "Closest Hit Parameter: " << hit.t << '\n';
+                //std::cout << "Closest Hit Surface Normal: " << hit.surface_normal.x() << " " << hit.surface_normal.y() << " " << hit.surface_normal.z() << '\n';
+                vec3<float> nf(hit.surface_normal.x(), hit.surface_normal.y(), hit.surface_normal.z());
+                color = 0.5F * (nf + white);
             }
             // TODO THINK ABOUT HOW TO CLEAN THIS LINE UP AND MAKE ACCESSING PIXEL DATA EASIER
             (*(render_image.img_array.get()))[i][j] = color; 
@@ -56,12 +50,12 @@ int main(int argc, char *argv[]){
     }
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "Render complete. Elapsed time: " ;
-    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[μs]" << std::endl;
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[μs]\n";
 
     if (render_image.writePFM(argv[1])<0) {
         std::perror("Writing image failed. Exiting.");
         exit(EXIT_FAILURE);
     }
-    std::cout << "Done writing image.\n";
+    std::cout << "Done writing image." << std::endl;
     return 0; // EXIT_SUCCESS;  
 }
